5b311829e414547c9ef86a1a955be49056afe937
praisemoses <praisemoses659@gmail.com>
2024-06-17 11:09:01 -0700

    4.4

--- conditionals.js
+++ conditionals.js
@@ -6,7 +6,15 @@ let other = document.querySelector(".other");
 
 button.addEventListener("click", function() {
     let state = document.querySelector(".state").value;
-
+    if (state==="California") {
+        californiaMessage.style.display = "block";
+    } else if (state==="Illinois") {
+        illinoisMessage.style.display = "block"
+    } else if (state==="New York"|| state==="New york"||state==="new York") {
+        newyorkMessage.style.display = "block" 
+    } else if (state==="Arizona") {
+        other.style.s = "block";
+    } else {}  
     // CODE ALONG
     // 1. Write an if statement that would display the california message if a user says they're from "California".
     
--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let button = document.querySelector("button");
 
 button.addEventListener("click", function() {
     let weather = document.querySelector(".weather").value;
+    if (weather="rainy") {
+        document.querySelector("<p>").innerHTML = "Bring an umbrella!";
+    } else {
+        document.querySelector(".<p>").innerHTML = "Enjoy the sun!";
+    }
     
     // 1. Write a conditional statement:
     //  - If the input is "rainy", change the text in the paragraph tag to "Bring an umbrella!"
